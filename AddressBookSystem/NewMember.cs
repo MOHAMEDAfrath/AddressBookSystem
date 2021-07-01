dab5eaa6af3e6ca661e20f913ddeb20eadb158ab
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-01 18:18:29 +0530

    [Afrath] Updated the file with Ability to add details

---
+++ AddressBookSystem/NewMember.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    /// <summary>
+    /// Getters and setter for getting setting details.
+    /// </summary>
+    class NewMember
+    {
+        public string firstname
+        {
+            get;
+            set;
+        }
+        public string lastname
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+        public string City
+        {
+            get;
+            set;
+        }
+        public string State
+        {
+            get;
+            set;
+        }
+        public string pincode
+        {
+            get;
+            set;
+        }
+        public string phonenumber
+        {
+            get;
+            set;
+        }
+    }
+}
