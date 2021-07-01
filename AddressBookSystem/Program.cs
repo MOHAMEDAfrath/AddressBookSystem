2b9a00d70f3242141b40f5775de4c048f761c4b5
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-01 19:35:54 +0530

    [Afrath] Updated the file with Modify Details

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -12,8 +12,9 @@ namespace AddressBookSystem
             while (true)
             {
                 Console.WriteLine("Enter an Option:");
-                Console.WriteLine("Enter 1 to Add a Member in a contact list:");
-                Console.WriteLine("Enter 2 to Print the Member in contact list:");
+                Console.WriteLine("Enter 1 to Add a Member in a contact list");
+                Console.WriteLine("Enter 2 to Print the Member in contact list");
+                Console.WriteLine("Enter 3 to Modify the contact details");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -24,6 +25,9 @@ namespace AddressBookSystem
                         AddressBook.ListContactPeople();
                         break;
                     case 3:
+                        AddressBook.Modify();
+                        break;
+                    case 4:
                         return;
 
                 }
